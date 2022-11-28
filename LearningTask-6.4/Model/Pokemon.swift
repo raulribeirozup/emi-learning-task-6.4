44f8ff651c38f4edf6e93de292280e98ce3adabe
Raul Crespo <113367054+raulribeirozup@users.noreply.github.com>
2022-11-27 21:00:02 -0300

    final commit

---
+++ LearningTask-6.4/Model/Pokemon.swift
@@ -0,0 +1,8 @@
+//
+//  Pokemon.swift
+//  LearningTask-6.4
+//
+//  Created by Raul Ribeiro on 27/11/22.
+//
+
+import Foundation
---
+++ LearningTask-6.4/Scenes/Pokedex/View/Componentes/PokemonViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokemonViewCell.swift
+//  LearningTask-6.4
+//
+//  Created by Raul Ribeiro on 27/11/22.
+//
+
+import UIKit
+
+class PokemonViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
